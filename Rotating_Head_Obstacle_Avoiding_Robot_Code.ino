11b1321178e74a92650802d0c33a7535426c70c2
Raagak <47781393+Raagak@users.noreply.github.com>
2019-03-06 23:29:24 +0530

    Add files via upload

---
+++ Rotating_Head_Obstacle_Avoiding_Robot_Code.ino
@@ -0,0 +1,131 @@
+#include <AFMotor.h>      
+#include <Servo.h>              
+#include <NewPing.h>      
+
+#define TRIG_PIN 9 
+#define ECHO_PIN 8 
+#define MAX_DISTANCE 300 
+#define MAX_SPEED 100
+#define MAX_SPEED_OFFSET 40 
+#define COLL_DIST 35 
+#define TURN_DIST COLL_DIST+15 
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE); 
+ 
+AF_DCMotor leftMotor(2, MOTOR12_8KHZ); 
+AF_DCMotor rightMotor(1, MOTOR12_8KHZ); 
+Servo myservo;  
+
+int leftDistance, rightDistance; 
+int curDist = 0;
+String motorSet = "";
+int speedSet = 0;
+
+
+void setup() {
+  myservo.attach(10);   
+  myservo.write(90); 
+  delay(1000); 
+ }
+
+void loop() {
+  myservo.write(90);  
+  delay(90);
+  curDist = readPing();  
+  if (curDist < COLL_DIST) {changePath();}  
+  moveForward();
+  delay(500);
+ }
+
+
+void changePath() {
+  moveStop();   
+  myservo.write(10);  
+    delay(500);
+    leftDistance = readPing(); 
+    delay(500);
+    myservo.write(180);  
+    delay(700);
+    rightDistance = readPing(); 
+    delay(500);
+    myservo.write(90); 
+    delay(100);
+    compareDistance();
+  }
+
+  
+void compareDistance()   
+{
+  if (leftDistance>rightDistance) 
+  {
+    turnLeft();
+  }
+  else if (rightDistance>leftDistance) 
+  {
+    turnRight();
+  }
+   else 
+  {
+    turnAround();
+  }
+}
+
+int readPing() { 
+  delay(70);   
+  unsigned int uS = sonar.ping();
+  int cm = uS/US_ROUNDTRIP_CM;
+  return cm;
+}
+
+void moveStop() {leftMotor.run(RELEASE); rightMotor.run(RELEASE);}  
+
+void moveForward() {
+    motorSet = "FORWARD";
+    leftMotor.run(FORWARD);      
+    rightMotor.run(FORWARD); 
+    for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2)    
+    
+  {
+    leftMotor.setSpeed(100);
+    rightMotor.setSpeed(100); 
+    delay(5);
+  }
+}
+
+void turnRight() {
+  motorSet = "RIGHT";
+  leftMotor.run(FORWARD);  
+  rightMotor.run(BACKWARD);   
+  rightMotor.setSpeed(speedSet+MAX_SPEED_OFFSET);      
+  delay(350);      
+  motorSet = "FORWARD";
+  leftMotor.run(FORWARD);      
+  rightMotor.run(FORWARD);
+        
+}  
+
+void turnLeft() {
+  motorSet = "LEFT" ;
+  leftMotor.run(BACKWARD);      
+  leftMotor.setSpeed(speedSet+MAX_SPEED_OFFSET);    
+  rightMotor.run(FORWARD);     
+  delay(350);  
+  motorSet = "FORWARD";
+  leftMotor.run(FORWARD);      
+  rightMotor.run(FORWARD);     
+ 
+}  
+
+void turnAround() {
+  motorSet = "TURNAROUND";
+  leftMotor.run(FORWARD);      
+  rightMotor.run(BACKWARD);    
+  rightMotor.setSpeed(speedSet+MAX_SPEED_OFFSET);      
+  
+  delay(700); 
+  motorSet = "FORWARD";
+  leftMotor.run(FORWARD);      
+  rightMotor.run(FORWARD);
+       
+}  
+
+
---
+++ sketch_feb23a.ino
@@ -0,0 +1,66 @@
+#define echopin  8 // echo pin
+#define trigpin 9 // Trigger pin
+
+
+int maximumRange = 30;
+long duration, distance;
+
+void setup() {
+  Serial.begin (9600);
+  pinMode (trigpin, OUTPUT);
+  pinMode (echopin, INPUT );
+  pinMode (4, OUTPUT);
+  pinMode (5, OUTPUT);
+  pinMode (13, OUTPUT);
+  pinMode (6, OUTPUT);
+  pinMode (7, OUTPUT);
+}
+
+void loop ()
+{
+
+  {
+    digitalWrite(trigpin,LOW);
+    delayMicroseconds(2);
+    
+    digitalWrite(trigpin,HIGH);
+    delayMicroseconds(10);
+    
+    duration=pulseIn (echopin,HIGH);
+    
+    distance= duration/58.2;
+    delay (50);
+    Serial.println(distance);
+  }
+  
+  if (distance >= 30 ){
+    digitalWrite(4,HIGH);
+    digitalWrite(5,HIGH);
+    digitalWrite(6,LOW);
+    digitalWrite(7,LOW);
+    delay (200);
+  }
+  
+  else if (distance >=15 && distance <= 25) {
+    digitalWrite (4,HIGH);
+    digitalWrite (5,LOW);
+    digitalWrite (6,LOW);
+    digitalWrite (7,LOW);
+    delay (1000);
+  }
+ else if (distance < 15){
+   digitalWrite (4, LOW);
+   digitalWrite (5, LOW);
+   digitalWrite (6,HIGH);
+   digitalWrite (7,HIGH);
+   delay (1000);
+   digitalWrite (4,LOW);
+   digitalWrite (5,HIGH);
+   digitalWrite (6,LOW);
+   digitalWrite (7, LOW);
+   delay (1000);
+   
+     
+ }
+
+}
